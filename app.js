ecbbcbba988092081a1aeeff036cdf39ee8e3039
SitiNurulUlumi <72804716+SitiNurulUlumi@users.noreply.github.com>
2021-03-14 00:39:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'crud'
+    database:'crudd'
 });
 connection.connect(function(error){
     if(!!error) console.log(error);
@@ -22,14 +22,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/',(req, res) =>{
+app.get('/',(req, res) => {
+    let sql = `SELECT username, no_hp, alamat, usernames FROM reseller INNER JOIN login ON reseller.id_admin=login.id_admin`;
+    connection.query(sql,(err, data) => {
+        if(err) throw err ;
+        res.render('user_index', {
+            title : 'DATA RESELLER DAN ADMIN',
+            seller : data
+        });
+    });
+});
+
+app.get('/admin',(req, res) =>{
+    //res.send('DATA RESELLER');
+    let sql = "SELECT * FROM login";
+    let query = connection.query(sql, (err, logs)=>{
+        if(err) throw err;
+        res.render('log_index',{
+            title : 'DATA ADMIN',
+            login : logs
+        });
+    });
+});
+
+app.get('/ress',(req, res) =>{
     //res.send('DATA RESELLER');
     let sql = "SELECT * FROM reseller";
-    let query = connection.query(sql, (err, rows)=>{
+    let query = connection.query(sql, (err, ress)=>{
         if(err) throw err;
-        res.render('user_index',{
+        res.render('ress_index',{
             title : 'DATA RESELLER',
-            reseller : rows
+            reseller : ress
         });
     });
 });
@@ -45,13 +68,13 @@ app.post('/save',(req, res)=>{
     let sql = "INSERT INTO reseller SET ?";
     let query = connection.query(sql, data, (err, results)=>{
         if(err) throw err;
-        res.redirect('/');
+        res.redirect('/ress');
     });
 });
 
-app.get('/edit/:sellerId',(req, res) => {
-    const sellerId = req.params.sellerId;
-    let sql = `Select * from reseller where id = ${sellerId}`;
+app.get('/edit/:id_reseller',(req, res) => {
+    const id_reseller = req.params.id_reseller;
+    let sql = `Select * from reseller where id_reseller = ${id_reseller}`;
     let query = connection.query(sql,(err, result) => {
         if(err) throw err;
         res.render('user_edit', {
@@ -62,24 +85,23 @@ app.get('/edit/:sellerId',(req, res) => {
 });
 
 app.post('/update',(req, res) => {
-    const sellerId = req.body.id;
-    let sql = "update reseller SET username='"+req.body.username+"',  email='"+req.body.email+"',  no_hp='"+req.body.no_hp+"', alamat='"+req.body.alamat+"', id_admin='"+req.body.id_admin+"' where id ="+sellerId;
+    const id_reseller = req.body.id_reseller;
+    let sql = "update reseller SET username='"+req.body.username+"',  email='"+req.body.email+"',  no_hp='"+req.body.no_hp+"', alamat='"+req.body.alamat+"', id_admin='"+req.body.id_admin+"' where id_reseller ="+id_reseller;
     let query = connection.query(sql,(err, results) => {
       if(err) throw err;
-      res.redirect('/');
+      res.redirect('/ress');
     });
 });
 
 ///delete
-app.get('/delete/:sellerId',(req,res) => {
-    const sellerId = req.params.sellerId;
-    let sql = `DELETE from reseller where id = ${sellerId}`;
+app.get('/delete/:id_reseller',(req,res) => {
+    const id_reseller = req.params.id_reseller;
+    let sql = `DELETE from reseller where id_reseller = ${id_reseller}`;
     let query = connection.query(sql,(err,result) => {
         if(err) throw err;
-        res.redirect('/');
+        res.redirect('/ress');
     });
 });
-
 
 //Server Listening
 app.listen(3000, ()=>{
